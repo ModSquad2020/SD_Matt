9d529b4c4ae35654bbf477f154fd33ebefe91ee2
Matthew Kozubov <matt.kozu@gmail.com>
2020-10-20 21:58:27 -0700

    First commit, has personal projects
    	-bpFileParser
    	Originally is supposed to parse tRNAs based on their
    	structures (bp is base pairs)
    
    	-m1A_test
    	contains my by hand files that produced the first
    	CM models
    
    	-mattstuff --> to be renamed
    	This has MODOMICS style downloaded CSVs that are
    	kind of awful. Henry's code should fix this issue,
    	but my code can manipulate and filter these formats
    	using pandas

---
+++ mattstuff/modomics_csv_tool.py
@@ -0,0 +1,11 @@
+'''
+Looks like we want the 5th column and the format wack as heck.
+I downloaded E.Coli tRNAs, visualized as Modomics Symbols, and 
+it looks like they have a pattern of 
+
+    name-of mod[show modidication pathway]\');return false">
+
+in front of every modification.Potentail use for regular expressions.
+
+    lowercase|number (for unknown amount of space)[show modification pathway]\');return true|false">
+'''
