0e2b5d14ebd3abb5a59ab2e14ae9a51ba6f1f39b
Matthew Kozubov <matt.kozu@gmail.com>
2020-10-28 13:09:32 -0700

    Deletes m1A files that belong in yeast_byhand

--- m1A_test/replace_dash.py
+++
@@ -1,6 +0,0 @@
-filename = '-trna_pos51_m1A_aligned.fa'
-with open(filename, 'r') as file:
-	with open('new_'+filename, 'w') as write_to:
-		for line in file.readlines():
-			line=line.replace('-','')
-			write_to.write(line)
