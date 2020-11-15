388567d4255d71100595fbc02abce52c14224e99
Matthew Kozubov <matt.kozu@gmail.com>
2020-11-15 15:52:08 -0800

    Creates a new Slicer within NNN_beta + more data
    * NNNSlicer_beta
    	This now has the CreateWindows class which
    	needs an input of a fasta sequence + secondary
    	structure + desired regions ['AA-stem','D-arm','Anticodon loop',
    	'T-arm', 'Introns'] and it will automatacilly
    	spit out the desired tRNA.
    
    	Bug: The regular expression that finds the loops shown by
    	<<>> doesn't get surrounding nucleotides. There is a disjoint
    	problem where even when all the regions are selected, a few
    	nucleotides may still appear as N's
    
    * fileConversion.py
    	This file now takes a .sto file as input from the commandline
    	and spits out the the same file name in .fa format and
    	extension.
    
    * Two .sto data files aligned to euk.cm

--- m1A_test/fileConversion.py
+++ m1A_test/fileConversion.py
@@ -1,5 +1,9 @@
 from Bio import SeqIO
+import sys
 
-filepath = "TestingNNN/"
-SeqIO.convert(filepath+'AlignedUsingEuk.sto', 'stockholm', filepath+'AlignedUsingEuk.fa', 'fasta')
-print('hello')
+print(sys.argv[1])
+fromfile = sys.argv[1]
+tofile = fromfile.split('.')[0]+'.fa'
+
+SeqIO.convert(fromfile, 'stockholm', tofile, 'fasta')
+print(f'Done: created {tofile}')
