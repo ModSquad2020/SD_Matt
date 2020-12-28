79be0b99b8e594ec0e4d460aa49574e2000bb5bd
Matthew Kozubov <matt.kozu@gmail.com>
2020-12-27 23:49:30 -0800

    FileTypeToolkit merges modomics with Andrew data
    The code is horrendous and uncommented, but in FileTypeToolkit.py
    is how I added the Modomics true positives to the Sacc Cere data.
    
    The only issues are that I cannot add modomics data to modifications
    we havent run with the pipeline, and it takes a pretty long time (180 s)
    to generate the isItSmaller.csv file.
    
    I have a complexity x^2 for my list searches so that is why the time is
    pretty awful.
    
    Whenever the database expands I think we will need to rerun this in
    order to add in the extra modomics data (will definitely need to find
    a faster way of doing this)

--- mattstuff/DataType/FileTypeToolkit.py
+++ mattstuff/DataType/FileTypeToolkit.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
+import sys
+sys.path.insert(1, "./Potentially massive folder")
 from modomicsDictionary import modomicsDict as modDic
+import csv
+from copy import deepcopy
 
 file = 'TESTresults.csv'
+file2 = "distilledModomics.csv"
 delim = ' '
 
 def addEmptyColumn(name):
@@ -17,14 +22,22 @@ def removeColumn(name):
 		df.drop(columns=name, inplace=True)
 		df.to_csv(file, sep=delim, index=False)
 
-with open("modomics_organisms.txt") as file:
-	with open("new_modomics_organisms.txt", 'w') as file2:
-		newSet = set()
-		for org in file.readlines():
-			newSet.add(org)
-		for item in newSet:
-			file2.write(item)
+#listOfMods = ''
+'''
+with open('mod_shortnames.txt', 'w') as file:
+	for key, values in modDic.items():
+		print(values['short_name'])
+		thing=values['short_name']
+		if thing not in 'AGCU':
+			try:
+				newThing = thing#.replace('(', "\\(").replace(')', "\\)")
+				listOfMods += f' {newThing}'
+				file.write(f'{thing}\n')
+			except UnicodeEncodeError:
+				pass
+	print(listOfMods)
 
+'''
 #for translating the dumb modomics format anticodons into cannonical bases
 originalBaseDic = {'τ': 'C', 'F': 'U', '∨': 'G', '∞': 'A', 'ς': 'G', '4': 'U', 
 					'α': 'U', 'ν': 'C', '`': 'A', '℘': 'U', 'T': 'U', 'R': 'G', 'D': 'U', 'μ': 'C', 
@@ -48,3 +61,55 @@ originalBaseDic = {'τ': 'C', 'F': 'U', '∨': 'G', '∞': 'A', 'ς': 'G', '4': 
 					'ϒ': 'pppN', 'Ξ': 'pppN', 'y': 'G', 'none': 'pppN', 'l': 'U', 'r': 'U', 'b': 'U', 
 					'm': 'pppN', 'Γ': 'U', 'f': 'U', 'h': 'U', 'Δ': 'U', '': 'pppN', 'e': 'A', 'Ϫ': 'A', 
 					'Ϩ': 'A', 'Ѷ': 'U', 'Ͽ': 'U', 'ÿ': 'A', '«': 'A', '£': 'A', '¡': 'C'}
+
+
+andrewList = []
+with open(file) as csvfile:
+	csvfile.readline()
+	andrewReader = csv.reader(csvfile, delimiter = delim)
+	for line in andrewReader:
+		andrewList.append(line)
+
+distilledModomics = []
+with open(file2) as csvfile:
+	csvfile.readline()
+	andrewReader = csv.reader(csvfile, delimiter = delim)
+	for line in andrewReader:
+		distilledModomics.append(line)
+
+
+num = 0
+newThing = deepcopy(andrewList)
+print(distilledModomics[0][9])
+
+for andrewItem in andrewList:
+	for modItem in distilledModomics:
+		if modItem[6] == 'Ini':
+			modItem[6] = 'iMet'
+
+		if andrewItem[0] == modItem[2]:
+			#if names are the same
+			if f'{modItem[6]}-{modItem[7]}-{modItem[8]}' == '-'.join( andrewItem[1].split('-')[0:3] ):
+				#if tRNAs are the same
+				if andrewItem[2].split('_')[2] == modItem[4]:
+					#if mod is the same
+					if andrewItem[2].split('_')[3] == modItem[5]:
+						#if position is the same
+						toAdd = [modItem[9], '+']
+						andrewItem.extend(toAdd)
+						break
+	try:
+		test = andrewItem[10] + ""
+	except IndexError:
+		toAdd = [None, '-']
+		andrewItem.extend(toAdd)
+'''
+for n, thing in enumerate( andrewList ):
+	print(n, thing, '\n')
+'''
+
+df = pd.DataFrame( andrewList, columns = ['Organism Name', 'tRNA', "Query CM", 'GC Content', "Description", "pos Score", "pos eVal", "neg Score", "neg eVal", "Modomics Sequence", "Modomcis Positive"] )
+df.to_csv("isItTheSame.csv", sep=delim, index=False)
+		
+
+	
